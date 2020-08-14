c366d2eca945822340969a12bc75dfed8fa09a02
Nathan Friend <nathan@gitlab.com>
2020-08-14 18:39:39 -0400

    Add tests for 404 page

--- tests/homepage.test.js
+++ tests/homepage.test.js
@@ -10,96 +10,149 @@ describe('https://nathanfriend.io', () => {
     expect(elementContent.trim().length).toBeGreaterThan(0);
   };
 
-  beforeAll(async () => {
-    await page.goto('https://nathanfriend.io');
-  });
+  describe('homepage content', () => {
+    let response;
 
-  it('has the correct title', async () => {
-    expect(await page.title()).toBe('Nathan Friend');
-  });
-
-  it('has the correct header', async () => {
-    expect(await getElementText('h1')).toBe('Nathan Friend');
-  });
-
-  it('has summary text', async () => {
-    await expectElementNotToBeEmpty('.site-description');
-  });
-
-  it('has a GitLab corner', async () => {
-    await expectElementToExist('.gitlab-corner');
-  });
-
-  describe('sidebar links', () => {
-    it.each`
-      display       | url
-      ${'About Me'} | ${'/about-me'}
-      ${'Projects'} | ${'/projects'}
-      ${'Blog'}     | ${'/all-posts'}
-    `('has a "$display" link that points to $url', async ({ display, url }) => {
-      const linkText = await getElementText(
-        `.sidebar-links-container a[href="${url}"]`,
-      );
-
-      expect(linkText).toBe(display);
+    beforeAll(async () => {
+      response = await page.goto('https://nathanfriend.io');
     });
-  });
 
-  describe('contact links', () => {
-    it.each`
-      tooltip                            | url
-      ${'Email: hello@nathanfriend.io'}  | ${'mailto:hello@nathanfriend.io'}
-      ${'GitLab: nfriend'}               | ${'https://gitlab.com/nfriend'}
-      ${'GitHub: nfriend'}               | ${'https://github.com/nfriend'}
-      ${'LinkedIn: nfriend'}             | ${'https://www.linkedin.com/in/nfriend/'}
-      ${'Stack Overflow: nathan-friend'} | ${'https://stackoverflow.com/users/1063392/nathan-friend'}
-      ${'Twitter: NathanAFriend'}        | ${'https://twitter.com/NathanAFriend'}
-      ${'Facebook: nathan.friend'}       | ${'https://www.facebook.com/nathan.friend'}
-    `(
-      'has a contact link with a tooltip of "$tooltip" that points to $url',
-      async ({ tooltip: expectedTooltip, url }) => {
-        const actualTooltip = await page.$eval(
-          `.profile-links-container a[href="${url}"]`,
-          (el) => el.dataset.originalTitle,
+    it('returns a 200 HTTP status code', async () => {
+      expect(response.status()).toBe(200);
+    });
+
+    it('has the correct title', async () => {
+      expect(await page.title()).toBe('Nathan Friend');
+    });
+
+    it('has the correct header', async () => {
+      expect(await getElementText('h1')).toBe('Nathan Friend');
+    });
+
+    it('has summary text', async () => {
+      await expectElementNotToBeEmpty('.site-description');
+    });
+
+    it('has a GitLab corner', async () => {
+      await expectElementToExist('.gitlab-corner');
+    });
+
+    describe('sidebar links', () => {
+      it.each`
+        display       | url
+        ${'About Me'} | ${'/about-me'}
+        ${'Projects'} | ${'/projects'}
+        ${'Blog'}     | ${'/all-posts'}
+      `(
+        'has a "$display" link that points to $url',
+        async ({ display, url }) => {
+          const linkText = await getElementText(
+            `.sidebar-links-container a[href="${url}"]`,
+          );
+
+          expect(linkText).toBe(display);
+        },
+      );
+    });
+
+    describe('contact links', () => {
+      it.each`
+        tooltip                            | url
+        ${'Email: hello@nathanfriend.io'}  | ${'mailto:hello@nathanfriend.io'}
+        ${'GitLab: nfriend'}               | ${'https://gitlab.com/nfriend'}
+        ${'GitHub: nfriend'}               | ${'https://github.com/nfriend'}
+        ${'LinkedIn: nfriend'}             | ${'https://www.linkedin.com/in/nfriend/'}
+        ${'Stack Overflow: nathan-friend'} | ${'https://stackoverflow.com/users/1063392/nathan-friend'}
+        ${'Twitter: NathanAFriend'}        | ${'https://twitter.com/NathanAFriend'}
+        ${'Facebook: nathan.friend'}       | ${'https://www.facebook.com/nathan.friend'}
+      `(
+        'has a contact link with a tooltip of "$tooltip" that points to $url',
+        async ({ tooltip: expectedTooltip, url }) => {
+          const actualTooltip = await page.$eval(
+            `.profile-links-container a[href="${url}"]`,
+            (el) => el.dataset.originalTitle,
+          );
+
+          expect(actualTooltip).toBe(expectedTooltip);
+        },
+      );
+    });
+
+    describe('content section', () => {
+      it("has the article's title, rendered as a link", async () => {
+        await expectElementNotToBeEmpty('.content-section a h1');
+      });
+
+      it("has the article's date and approximate reading time", async () => {
+        const articleDateAndLength = await getElementText(
+          '.content-section > p',
         );
 
-        expect(actualTooltip).toBe(expectedTooltip);
-      },
-    );
-  });
+        // Should look something like "November 16, 2018 | 2 minutes to read"
+        const regex = /[a-z]+ \d{1,2}, 20\d{2}\s+\|\s+.*to read/i;
+        expect(articleDateAndLength).toMatch(regex);
+      });
 
-  describe('content section', () => {
-    it("has the article's title, rendered as a link", async () => {
-      await expectElementNotToBeEmpty('.content-section a h1');
+      it("has the article's content", async () => {
+        await expectElementNotToBeEmpty('.content-section .content-container');
+      });
     });
 
-    it("has the article's date and approximate reading time", async () => {
-      const articleDateAndLength = await getElementText('.content-section > p');
-
-      // Should look something like "November 16, 2018 | 2 minutes to read"
-      const regex = /[a-z]+ \d{1,2}, 20\d{2}\s+\|\s+.*to read/i;
-      expect(articleDateAndLength).toMatch(regex);
+    it('has a search bar', async () => {
+      await expectElementToExist('#footer-search-bar');
     });
 
-    it("has the article's content", async () => {
-      await expectElementNotToBeEmpty('.content-section .content-container');
+    it('has a copyright info', async () => {
+      const copyrightText = await getElementText('#legal');
+      expect(copyrightText).toContain('Copyright Nathan Friend © 2012');
+    });
+
+    it('has a link to the "Attributions" pag', async () => {
+      const attributionsLinkText = await getElementText(
+        `#legal a[href="/attributions"]`,
+      );
+
+      expect(attributionsLinkText).toBe('Attributions');
+    });
+
+    describe('404 behavior', () => {
+      it("has the article's title, rendered as a link", async () => {
+        await expectElementNotToBeEmpty('.content-section a h1');
+      });
+
+      it("has the article's date and approximate reading time", async () => {
+        const articleDateAndLength = await getElementText(
+          '.content-section > p',
+        );
+
+        // Should look something like "November 16, 2018 | 2 minutes to read"
+        const regex = /[a-z]+ \d{1,2}, 20\d{2}\s+\|\s+.*to read/i;
+        expect(articleDateAndLength).toMatch(regex);
+      });
+
+      it("has the article's content", async () => {
+        await expectElementNotToBeEmpty('.content-section .content-container');
+      });
     });
   });
 
-  it('has a search bar', async () => {
-    await expectElementToExist('#footer-search-bar');
-  });
+  describe('404 behavior', () => {
+    let response;
 
-  it('has a copyright info', async () => {
-    const copyrightText = await getElementText('#legal');
-    expect(copyrightText).toContain('Copyright Nathan Friend © 2012');
-  });
+    beforeAll(async () => {
+      response = await page.goto('https://nathanfriend.io/not-a-real-page');
+    });
 
-  it('has a link to the "Attributions" pag', async () => {
-    const attributionsLinkText = await getElementText(
-      `#legal a[href="/attributions"]`,
-    );
+    it('returns a 404 HTTP status code', async () => {
+      expect(response.status()).toBe(404);
+    });
 
-    expect(attributionsLinkText).toBe('Attributions');
+    it('has 404 in the title', async () => {
+      expect(await page.title()).toBe('404');
+    });
+
+    it('has a 404 header', async () => {
+      expect(await getElementText('.content-section h1')).toBe('404');
+    });
   });
 });
