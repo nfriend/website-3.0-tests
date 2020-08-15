5d425f7437930d6ef69408f207c070cc99851bd6
Nathan Friend <nathan@gitlab.com>
2020-08-15 14:25:59 -0400

    Add inspirograph tests

---
+++ tests/inspirograph.test.js
@@ -0,0 +1,52 @@
+const {
+  getElementText,
+  expectElementNotToBeEmpty,
+  expectElementToExist,
+} = require('./util');
+
+describe('Inspirograph', () => {
+  let response;
+
+  describe('main page', () => {
+    beforeAll(async () => {
+      response = await page.goto('https://nathanfriend.io/inspirograph');
+    });
+
+    it('returns a 200', () => {
+      expect(response.status()).toBe(200);
+    });
+
+    it('renders a gear', async () => {
+      await expectElementToExist('.gear.ring-gear path');
+    });
+  });
+
+  describe('gallery', () => {
+    beforeAll(async () => {
+      response = await page.goto(
+        'https://nathanfriend.io/inspirograph/gallery',
+      );
+
+      await page.waitForSelector('.image-thumbnail');
+    });
+
+    it('returns a 200', () => {
+      expect(response.status()).toBe(200);
+    });
+
+    it('renders 72 gallery images', async () => {
+      const imageCount = await page.$$eval(
+        '.image-thumbnail',
+        (images) => images.length,
+      );
+
+      expect(imageCount).toBe(72);
+    });
+
+    it('renders a pagination', async () => {
+      const pageCount = await getElementText('.ellipsis-container + div');
+
+      expect(parseInt(pageCount, 10)).toBeGreaterThan(3500);
+    });
+  });
+});
